73e5978eb73749d994cb62b9947ea61470304890
Murat Duran <mudu@mudu.com.tr>
2025-03-14 03:02:09 +0300

    2 changes by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,18 @@ export default defineAppConfig({
     colors: {
       primary: 'blue',
       neutral: 'slate'
+    },
+
+    gray: 'stone',
+    primary: 'sky',
+
+    icons: {
+      search: 'i-material-symbols-account-box-sharp',
+      dark: 'i-material-symbols-dark-mode',
+      light: 'i-material-symbols-lightbulb-rounded',
+      hash: 'i-material-symbols-link-rounded',
+      chevron: 'i-material-symbols-chevron-right',
+      external: 'i-material-symbols-domain-verification-outline-sharp'
     }
   }
 })
